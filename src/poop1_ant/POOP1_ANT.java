55405a98b4b8dbf1715c890c67f842353fed5881
JAZM610 <jazm.610@gmail.com>
2024-09-26 22:43:50 -0600

    Creación de la práctica

---
+++ src/poop1_ant/POOP1_ANT.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poop1_ant; // Paquete que contiene la clase POOP1_ANT
+
+/**
+ * Clase principal del programa POOP1_ANT.
+ * 
+ * @author DLTM
+ */
+public class POOP1_ANT {
+    
+    /**
+     * Método principal que se ejecuta al iniciar el programa.
+     * 
+     * @param args Argumentos de la línea de comandos (no utilizados en este programa).
+     */
+    public static void main(String[] args) {
+        // Imprime un mensaje de saludo en la consola
+        System.out.println("Hola Mundo con ANT");
+        System.out.println("Desde POO G3");
+        
+        // Declaración y asignación de variables
+        int a = 3; // Variable primitiva de tipo entero
+        Integer b = 3; // Objeto Integer que envuelve un valor entero
+        String cadena = new String("hola"); // Objeto String que contiene la cadena "hola"
+        
+        // Imprime los valores de las variables en la consola
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(cadena);
+    }
+}
