38c157ce33b8de25e66c78472a0a56d39d06766f
JAZM610 <jazm.610@gmail.com>
2024-09-26 23:01:07 -0600

    Creación de práctica

---
+++ src/main/java/com/mycompany/poop1_maven/POOP1_MAVEN.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poop1_maven; // Paquete que contiene la clase POOP1_MAVEN
+
+/**
+ * Clase principal del programa POOP1_MAVEN.
+ * 
+ * @author DLTM
+ */
+public class POOP1_MAVEN {
+
+    /**
+     * Método principal que se ejecuta al iniciar el programa.
+     * 
+     * @param args Argumentos de la línea de comandos (no utilizados en este programa).
+     */
+    public static void main(String[] args) {
+        // Imprime un mensaje de saludo en la consola
+        System.out.println("Hola Mundo!");
+    }
+}
